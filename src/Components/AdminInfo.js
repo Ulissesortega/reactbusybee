815215843cc78158527dbec67617c7e6b5e9ee0e
Ulises <ulisses.ortega@hotmail.com>
2023-03-29 09:28:54 -0700

    update

--- src/Components/AdminInfo.js
+++ src/Components/AdminInfo.js
@@ -5,47 +5,56 @@ import { Container, Row, Col, Form } from 'react-bootstrap';
 
 export default function AdminInfo() {
   return (
-    <Container>
-      <Row>
-        <Col>
-          <h1 className='text-center mt-5 main-title'>Busy Bee!</h1>
-        </Col>
-      </Row>
+    <div className="Blackcontainer">
+      <div className="rectangle">
+        <div className="inner-rectangle">
+          <Container>
+            <Row>
+              <Col>
+                <h1 className='text-center mt-5 main-title'>Busy Bee!</h1>
+              </Col>
+            </Row>
 
-      <Row>
-        <Col>
-          <p className='top-text text-center'>Please Enter<br />Your Information</p>
-        </Col>
-      </Row>
+            <Row>
+              <Col>
+                <p className='top-text text-center'>Please Enter<br />Your Information</p>
+              </Col>
+            </Row>
 
-      <Row>
-        <Col className='text-center'>
-          <Form.Label className='input-label' htmlFor='firstName'>First Name:</Form.Label>
-          <Form.Control className='text-input rounded-pill w-75 mx-auto border border-primary text-center' type="text" placeholder="" />
-        </Col>
-      </Row>
+            <Row>
+              <Col className='text-center'>
+                <Form.Label className='input-label' htmlFor='firstName'>First Name:</Form.Label>
+                <Form.Control className='text-input rounded-pill w-75 mx-auto border border-primary text-center' type="text" placeholder="" />
+              </Col>
+            </Row>
 
-      <Row>
-        <Col className='text-center'>
-          <Form.Label className='input-label' htmlFor='LastName'>Last Name:</Form.Label>
-          <Form.Control className='text-input rounded-pill w-75 mx-auto border border-primary text-center' type="text" placeholder="" />
-        </Col>
-      </Row>
+            <Row>
+              <Col className='text-center'>
+                <Form.Label className='input-label' htmlFor='LastName'>Last Name:</Form.Label>
+                <Form.Control className='text-input rounded-pill w-75 mx-auto border border-primary text-center' type="text" placeholder="" />
+              </Col>
+            </Row>
 
-      <Row>
-        <Col className='text-center'>
-          <Form.Label className='input-label' htmlFor='Email'>Email Address:</Form.Label>
-          <Form.Control className='text-input rounded-pill w-75 mx-auto border border-primary text-center' type="text" placeholder="" />
-        </Col>
-      </Row>
-      <Row>
-        <Col className='text-center'>
-          <Form.Label className='input-label' htmlFor='Password'>Password:</Form.Label>
-          <Form.Control className='text-input rounded-pill w-75 mx-auto border border-primary text-center' type="text" placeholder="" />
-        </Col>
-      </Row>
+            <Row>
+              <Col className='text-center'>
+                <Form.Label className='input-label' htmlFor='Email'>Email Address:</Form.Label>
+                <Form.Control className='text-input rounded-pill w-75 mx-auto border border-primary text-center' type="text" placeholder="" />
+              </Col>
+            </Row>
+            <Row>
+              <Col className='text-center'>
+                <Form.Label className='input-label' htmlFor='Password'>Password:</Form.Label>
+                <Form.Control className='text-input rounded-pill w-75 mx-auto border border-primary text-center' type="text" placeholder="" />
+              </Col>
+            </Row>
 
 
-    </Container>
+          </Container>
+        </div>
+      </div>
+    </div>
+
+
+
   )
 }
